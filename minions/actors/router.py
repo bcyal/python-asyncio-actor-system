ff04228b15f4ad5708879be513bbc7a130de0b76
bcyal <bcyal@wtf.com>
2022-10-26 19:40:21 +0200

    fixed router issue, tests left to write for router

--- minions/actors/router.py
+++ minions/actors/router.py
@@ -1,23 +1,25 @@
 import asyncio
 
+from minions.actors.actor import Actor
 from minions.actors.supervisor import Supervisor
 
 
 class Router(Supervisor):
-    
-    ## FIXME: NOT WORKING !!!!!!
-    ## Router is blocking (Quesize of Router is going up)
-    ## and throwing CanceledError after Cancel
-    ## probably need to redo the _handle method instead of
-    ## handle_message-method
-    ## Custom routers are doing what they are supposed to do
-    async def handle_message(self, message, sender):
-        """
-        Router only need this handle_message-method
-        """
+    def __call__(
+        self, 
+        message, 
+        sender
+        ):
+        if self.status is not Actor.RUNNING or self._worker.done():
+            raise asyncio.CancelledError()
+        if any(
+            ch.status is not Actor.RUNNING 
+            or ch._worker.done() for ch in self._children
+            ):
+            raise asyncio.CancelledError()
         self._logger.debug(
             f"{self} received message {message} for routing"
-        )
+        )        
         try:
             target = self._route(message,sender)
         except Exception as err:
@@ -30,7 +32,8 @@ class Router(Supervisor):
             f"{self} is handing the message {message} "\
             f"from {sender} to {target}."
         )
-        result = await target(message,sender)
+        result = target._loop.create_future()
+        target._inbox.put_nowait((message, sender, result))
         return result
     
     def _route(self, message, sender):
