d89afbc323f84d25eb6c440e59ee9b22a2d37ae9
bcyal <bcyal@wtf.com>
2023-04-06 15:31:27 +0200

    identation fix

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
 import os
 import sys
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
 
 if sys.version_info < (3, 7, 0):
     raise RuntimeError(
-	    "Gru says, that minions require Python 3.7.0+"
-	)
+        "Gru says, that minions require Python 3.7.0+"
+    )
 
 
 setup(
-	name = "python-asyncio-actor-model",
-	version = "0.1",
-	author = "Bekir Can Yalcin",
-	author_email = "wtf@wtf.com",
-	description = ("asyncio based actor model"),
-	license = "WTFPL",
-	url = "wtf.com",
-	long_description="asyncio based actor model",
-	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Topic :: Utilities",
-		"License :: OSI Approved :: WTFPL License",
-	],
-	packages=[
-        'minions.actors',
-        'minions.actors.custom.routers',
-        'minions.actors.custom.sources',
-	],
-	install_requires=[
-		"falcon",
-		"uvicorn"
-	]
+    name = "python-asyncio-actor-model",
+    version = "0.1",
+    author = "Bekir Can Yalcin",
+    author_email = "wtf@wtf.com",
+    description = ("asyncio based actor model"),
+    license = "WTFPL",
+    url = "wtf.com",
+    long_description="asyncio based actor model",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: WTFPL License",
+    ],
+    packages=[
+        "minions.actors",
+        "minions.actors.custom.routers",
+        "minions.actors.custom.sources",
+    ],
+    install_requires=[
+        "falcon",
+        "uvicorn"
+    ]
 )
